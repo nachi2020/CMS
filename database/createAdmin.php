c89f03758fc22d1fa8c21750100ad3f9de590ef2
nachi <nachi2020>
2020-05-11 01:06:41 +0300

    modified creatingAdmin

--- database/createAdmin.php
+++ database/createAdmin.php
@@ -14,7 +14,7 @@ $role = 1;
 $emailVerifiedAt = NULL;
 $createdAt = NULL;
 
-$sql = "INSERT INTO users(f_name, l_name, email, password, role_id, email_verified_at, created_at) VALUES('$first', '$last', '$mail', SHA('$pwd'), '$role', '$emailVerifiedAt', '$createdAt')";
+$sql = "INSERT INTO users('f_name', 'l_name', 'email', 'password', 'role_id', 'email_verified_at', 'created_at') VALUES('$first', '$last', '$mail', SHA('$pwd'), '$role', '$emailVerifiedAt', '$createdAt')";
 $result = mysqli_query($conn,$sql);
 
 ?>
