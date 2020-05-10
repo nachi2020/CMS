e329c0c14044dc4a6181f48b25f187277fc35967
nachi <nachi2020>
2020-05-11 01:56:53 +0300

    login

--- database/createAdmin.php
+++ database/createAdmin.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+include "dbconnection.php";
 $first = "Nasra";
 $last = "Rashid";
 $mail = "nachirashid@gmail.com";
@@ -8,6 +9,6 @@ $role = 1;
 
 $sql = "INSERT INTO users(`f_name`, `l_name`, `email`, `password`, `role_id`, `email_verified_at`, `created_at`) VALUES('$first', '$last', '$mail', SHA('$pwd'), '$role', NOW(), NOW())";
 $result = mysqli_query($conn,$sql);
-include "dbconnection.php";
+
 
 ?>
---
+++ public/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="register.php">
+	<label>Email: <input type="Email" name="email"></label><br>
+	<label>Password: <input type="password" name="password"></label><br>
+	<input type="submit" name="login" value="Login"><br><br>
+
+</form>
+</body>
+</html>
