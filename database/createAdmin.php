389f0c56670dd5b4802481e28343fa20e275a64f
nachi <nachi2020>
2020-05-11 01:26:38 +0300

    include

--- database/createAdmin.php
+++ database/createAdmin.php
@@ -8,5 +8,6 @@ $role = 1;
 
 $sql = "INSERT INTO users(`f_name`, `l_name`, `email`, `password`, `role_id`, `email_verified_at`, `created_at`) VALUES('$first', '$last', '$mail', SHA('$pwd'), '$role', NOW(), NOW())";
 $result = mysqli_query($conn,$sql);
+include "dbconnection.php";
 
 ?>
