d0aae936e2b04d38368e0e6d28c3a43da6bce5d3
Gisacc54 <giftisacc54@gmail.com>
2020-05-18 03:08:14 +0300

    fix addmin dashboard error

--- public/adminDashboard.php
+++ public/adminDashboard.php
@@ -37,7 +37,7 @@ $result = mysqli_query($conn, $sql);
 while($row = mysqli_fetch_array($result)){
   ?>
       <tr>
-        <td><?php echo $row['user_id'];?></td>
+        <td><?php echo $row['id'];?></td>
         <td><?php echo $row['f_name']."    ".$row['l_name'];?></td>
         <td><?php echo $row['email'];?></td>
         <td><?php echo $row['role_id'];?></td>
