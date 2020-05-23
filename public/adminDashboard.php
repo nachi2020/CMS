26eefab4e2c90833619225ef1af147d59ff28e5c
Gisacc54 <giftisacc54@gmail.com>
2020-05-23 23:29:13 +0300

    add navigation

--- public/adminDashboard.php
+++ public/adminDashboard.php
@@ -11,12 +11,36 @@
     <link rel="stylesheet" type="text/css" href="../resources/css/main.css">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">CMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Setting</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          <a href="#" style="text-decoration: none;">
+            <svg class="bi bi-person-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
+            </svg> {{Name}}
+          </a>
+        </span>
+      </div>
+    </nav>
 
-<div class="container">
-  <a class="btn btn-primary" href="adduser.php">Add User</a>
-</div>
 
  <div class="container">
+   <a class="btn btn-primary adduser float-right" href="adduser.php">Add User</a>
   <div class="table-div">
   <table class="table table-bordered" border="1px">
    <thead>
